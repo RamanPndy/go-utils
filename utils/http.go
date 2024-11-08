e57391a5b0d0b7d64624df25d544d333d3708e8b
Raman Pandey <raman.pndy@gmail.com>
2024-11-08 16:28:53 +0530

    updated github workflows (#6)
    * updated github workflows
    
    * path fix
    
    * script execution permission fix
    
    * updated UTs
    
    * fixing UT
    
    * fixing UTs
    
    * added permissions
    
    * added release workflow
    
    * static workflow fix
    
    * lint
    
    * static workflow changes
    
    * go version changes

--- utils/http.go
+++ utils/http.go
@@ -2,12 +2,16 @@ package goutils
 
 import (
 	urlpkg "net/url"
+	"strings"
 )
 
 func EncodeQueryParams(url string, keyValue map[string]string) string {
 	queryParams := urlpkg.Values{}
 	for key, value := range keyValue {
 		queryParams.Add(key, value)
+	}
+	if !strings.HasSuffix(url, "?") {
+		url += "?"
 	}
 	return url + queryParams.Encode()
 }
